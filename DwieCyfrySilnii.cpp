ca9d5515dbd9393d843812d451e0f949a209acdd
TomaszP17 <70948711+TomaszP17@users.noreply.github.com>
2021-03-07 15:04:36 +0100

    Add files via upload

---
+++ DwieCyfrySilnii.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+
+int potega(int x, int y)
+{
+    return pow(x, y);
+}
+
+int main()
+{
+    int podstawa, wykladnik, n;
+    std::cin >> n;
+    if (n >= 10 || n <= 1)
+        return 0;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> podstawa >> wykladnik;
+        std::cout << (potega(podstawa, wykladnik)%10) << '\n';
+    }
+    return 0;
+}
