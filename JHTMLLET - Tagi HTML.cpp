ce3c147258240c6962982d3764e2c253daed2e68
TomaszP17 <70948711+TomaszP17@users.noreply.github.com>
2021-03-07 15:48:42 +0100

    Add files via upload

---
+++ JHTMLLET - Tagi HTML.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string napis;
+    while (getline(std::cin, napis))
+    {
+        for (int i = 0; i < napis.length(); i++)
+        {
+            if (napis[i] == '<')
+            {
+                while (napis[i] != '>')
+                {
+                    i++;
+                    napis[i] = toupper(napis[i]);
+                }
+            }
+        }
+        std::cout << napis << '\n';
+    }
+}
+
