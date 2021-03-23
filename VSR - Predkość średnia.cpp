4645c0f7b91ad108927d63c4a7b169f1cc7d21c2
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-03-23 19:19:30 +0100

    Add files via upload

---
+++ VSR - Predkość średnia.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main()
+{
+    int testy, liczba1, liczba2,  srednia = 0;
+    std::cin >> testy;
+    for (int i = 0; i < testy; i++)
+    {
+        std::cin >> liczba1 >> liczba2;
+        srednia = (2 * liczba1 * liczba2) / (liczba1 + liczba2);
+        std::cout << srednia << '\n';
+        srednia = 0;
+    }
+}
