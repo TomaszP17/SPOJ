549751146eaddf2f5e593087ac834ea118c2e99d
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-03-23 19:14:59 +0100

    Add files via upload

---
+++ KC010 - Zliczanie liczb i wyrazów.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string zdanie;
+    while (getline(std::cin, zdanie))
+    {
+        int slow = 0;
+        int liczb = 0;
+
+        for (int i = 0; i < zdanie.length(); i++)
+        {
+            if (zdanie[i] > 64 && zdanie[i] < 91 || zdanie[i]>96 && zdanie[i] < 123)
+            {
+                ++slow;
+
+                while (zdanie[i] != ' ' && i < zdanie.length())
+                    i++;
+            }
+            else if (zdanie[i] > 47 && zdanie[i] < 58)
+            {
+                ++liczb;
+                while (zdanie[i] != ' ' && i < zdanie.length())
+                {
+                    i++;
+                }
+            }
+        }
+        std::cout << liczb << " " << slow << '\n';
+    }
+}
