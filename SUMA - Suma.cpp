1854e1526ba26e1e8c64dfad2fb25fe65eb7ac0c
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-03-23 19:17:25 +0100

    Add files via upload

---
+++ SUMA - Suma.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int i = 0, liczba, suma = 0;
+	while (std::cin >> liczba)
+	{
+		if (std::cin.eof()) break;
+		suma += liczba;
+		std::cout << suma << '\n';
+		i++;
+	}
+}
