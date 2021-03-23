39206e427097e127eca2e4d293e0271c80ee0ca7
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-03-23 19:15:30 +0100

    Add files via upload

---
+++ MWPZ06D - Imieniny.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int D, L, C, y;
+    
+    std::cin >> D;
+
+    for (int i = 0; i < D; i++)
+    {
+        std::cin >> L >> C;
+
+
+        if (L == 1) std::cout << "TAK" << '\n';
+        else
+        {
+            y = C % (L - 1);
+
+            if (y > 0) std::cout << "TAK\n";
+            else std::cout << "NIE\n";
+        }
+    }
+}
