eb0fbeb1b103c1249a45f6e8a5f0be25769a3369
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:36:15 +0200

    Add files via upload

---
+++ PASTAB2 - Warunek w tablicy.cpp
@@ -0,0 +1,40 @@
+﻿// PASTAB2 - Warunek w tablicy
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int tab[t];
+    for (int i = 0;i < t;i++)
+        tab[i] = 0;
+    for (int i = 0;i < t;i++)
+    {
+        int x;
+        cin >> x;
+        tab[i] = x;
+    }
+    char znak;
+    cin >> znak;
+    int liczba;
+    cin >> liczba;
+    if (znak == '>')
+    {
+        for (int i = 0;i < t;i++)
+        {
+            if (tab[i] > liczba)
+                cout << tab[i] << endl;
+        }
+    }
+    else
+    {
+        for (int i = 0;i < t;i++)
+        {
+            if (tab[i] < liczba)
+                cout << tab[i] << endl;
+        }
+    }
+    return 0;
+}
