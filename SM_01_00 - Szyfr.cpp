b48526c63374268379024d77becb9626c1161760
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:39:21 +0200

    Add files via upload

---
+++ SM_01_00 - Szyfr.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i, l, testy;
+    char wyraz;
+
+    cin >> testy;
+    cin.get(wyraz);
+    while (testy--)
+    {
+        i = 4; l = 0;
+        while (cin.get(wyraz))
+        {
+            if (wyraz == '\n') break;
+            if (i == -1)
+            {
+                cout << (char)(l + 65);
+                i = 4;
+                l = 0;
+            }
+            l += (wyraz - '0') * pow(2, i);
+            i--;
+        }
+        cout << (char)(l + 65) << endl;
+    }
+}
