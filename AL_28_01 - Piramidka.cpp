be1019c46b3f3fd888f72e61991dca7aa42feb5c
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:26:10 +0200

    Add files via upload

---
+++ AL_28_01 - Piramidka.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int dlugosc;
+    std::cin >> dlugosc;
+    std::string wyraz;
+    std::cin >> wyraz;
+    int srodek = wyraz.length() / 2;
+    int n = 0;
+    for (;;)
+    {
+        for (int i = 0; i < wyraz.length(); i++)
+        {
+            if ((i < srodek) && (i < (srodek - n)) || (i > (srodek) && (i > srodek + n))) std::cout << '*';
+            else std::cout << wyraz[i];
+        }
+        if ((srodek - n) == 0) break;
+        else
+        {
+            n++;
+            std::cout << '\n';
+        }
+    }
+}
