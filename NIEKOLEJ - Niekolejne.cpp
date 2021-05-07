2929d2da1e88eb0229f44f41a474f883d063c6b3
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:34:36 +0200

    Add files via upload

---
+++ NIEKOLEJ - Niekolejne.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int liczba;
+    std::cin >> liczba;
+
+    if (liczba == 0)
+    {
+        return 0;
+    }
+    else if (liczba == 2 || liczba == 1)
+    {
+        std::cout << "NIE";
+        return 0;
+    }
+
+    for (int i = 1; i <= liczba; i += 2)
+        std::cout << i << " ";
+    for (int i = 0; i <= liczba; i += 2)
+        if (i != liczba)
+            std::cout << i << " ";
+        else
+            std::cout << i;
+}
