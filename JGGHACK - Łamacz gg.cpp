771b1a0e471cea21c34f8f43d4afe6a8569df94b
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:30:58 +0200

    Add files via upload

---
+++ JGGHACK - Łamacz gg.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+std::string Lamacz(std::string slowo)
+{
+    std::string haslo;
+
+    for (int i = 0; i < slowo.length(); i += 2)
+    {
+        haslo += (slowo[i] - 'A') + (slowo[i + 1] - 'A') * 16;
+    }
+    return haslo;
+}
+
+int main()
+{
+    std::string wejscie;
+
+    while (getline(std::cin, wejscie))
+        std::cout << Lamacz(wejscie) << '\n';
+}
