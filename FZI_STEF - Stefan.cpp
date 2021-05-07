43552f1a5a4f44592ea9cc6fbf018d6a13e265ba
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:30:29 +0200

    Add files via upload

---
+++ FZI_STEF - Stefan.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int testy;
+    std::cin >> testy;
+
+    int wszystko = 0, aktualne = 0;
+    int tab[testy];
+
+    for (int i = 0; i < testy; i++)
+    {
+        std::cin >> tab[i];
+
+        aktualne += tab[i];
+        if (aktualne < 0) aktualne = 0;
+        else
+            if (aktualne > wszystko) wszystko = aktualne;
+    }
+    std::cout << wszystko << '\n';
+}
