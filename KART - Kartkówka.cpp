2d2df9fbfe79793f678d4562c41052b89ed2c5fc
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:32:37 +0200

    Add files via upload

---
+++ KART - Kartkówka.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int testy;
+    std::cin >> testy;
+
+    for (int i = 0; i < testy; i++)
+    {
+        int liczba, x = 5, zero = 0;
+        std::cin >> liczba;
+
+        while (liczba >= x)
+        {
+            zero += liczba / x;
+            x *= 5;
+        }
+        std::cout << zero << '\n';
+    }
+}
