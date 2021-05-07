b5dcad692f4bd7b4bbf3e8d36006295ea1d03393
Bemowianin03 <70948711+TomaszP17@users.noreply.github.com>
2021-05-07 13:27:41 +0200

    Add files via upload

---
+++ BICAKE - Birthday cake.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    long long wprowadzona;
+
+    while (std::cin >> wprowadzona)
+    {
+        long long kawalek = 1;
+        int i = 1;
+
+        while (kawalek < wprowadzona)
+        {
+            kawalek += i;
+            i++;
+        }
+        std::cout << i - 1 << '\n';
+    }
+}
